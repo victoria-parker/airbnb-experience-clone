fb363150f1f29cb8f4072def4b0fdf1479edaa41
victoria-parker <victoriaparkerb@gmail.com>
2022-01-10 15:48:56 +0000

    image bug corrected in hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,10 @@
 import React from "react"
+import heroGrid from "../images/photo-grid.png"
 
 export default function Hero(){
     return (
         <div className="hero">
-            <img src={"../images/photo-grid.png"} alt="hero grid"/>
+            <img src={heroGrid} alt="hero grid"/>
             <div className="text">
                 <h1>Online Experiences</h1>
                 <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
